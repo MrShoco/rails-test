c880946c18b877e408062cf08f3c1f66f58ffb19
mrshoco <karenmanuk@yandex.ru>
2015-06-08 04:10:42 +0300

    Add some specs

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe ImageController, type: :controller do
+RSpec.describe ImagesController, type: :controller do
+  let(:user) { create(:user) }
+  let(:photo) { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'images', 'test.jpg'), 'image/jpg') }
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  context '#create' do
+    context 'when user is not logged in' do
+    end
+
+    context 'when image is not provided' do
+    end
+
+    context 'when everything is all right' do
+      before { allow(subject).to receive(:current_user).and_return(user) }
+      before { post :create, image: { file: photo } }
+
+      it { expect(response).to redirect_to(user_path(user.id)) }
     end
   end
-
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,21 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
-  let(:user) { create(:user) }
-
-  before { allow(subject).to receive(:current_user).and_return(user) }
-
   context "#index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    context 'when there are no users' do
+      before { get :index }
+
+      it { expect(response).to render_template(:index) }
+      it { expect(assigns(:users)).to be_empty }
+    end
+
+    context 'when there are some users' do
+      let(:users) { create_list(:user, 10) }
+
+      before { get :index }
+
+      it { expect(assigns(:users)).to match users }
     end
   end
 
   context "#show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    let(:user) { create(:user) }
+
+    context 'when there are no users' do
+      before { get :show, id: user.id }
+
+      it { expect(response).to render_template(:show) }
+      it { expect(assigns(:user)).to eq user }
     end
   end
 
